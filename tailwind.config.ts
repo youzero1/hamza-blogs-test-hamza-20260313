92545a94a2c9e45b486160ef55e46ebe3e0c93bd
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-13 21:20:40 +0500

    Add tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"]
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: "100%"
+          }
+        }
+      }
+    }
+  },
+  plugins: []
+};
+export default config;
